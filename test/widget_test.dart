23a54b9c6284c435997aa3fb6952bff5e5da3b0f
Yayo <geramail28@gmail.com>
2021-11-25 11:57:48 +0800

    Update packages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,9 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_connectivity_tutorial/src/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_connectivity_tutorial/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
